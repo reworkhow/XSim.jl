5625307b491db94a0758b10d333a1dcaa119b89d
Hao Cheng <reworkhow@gmail.com>
2015-12-14 22:48:29 -0600

    Update moreInfo.jl

--- src/output/moreInfo.jl
+++ src/output/moreInfo.jl
@@ -3,11 +3,12 @@
 function find_haplotype_founder(animals::Cohort)
     haps=[]
     for i in animals.animalCohort
-        for j in [i.genomePat,i.genomeMat]
+        for j in [i.genomePat;i.genomeMat]
             for k in j.ori
                 push!(haps,k)
             end
         end
     end
     println("Identifying founders for haplotype segments ")
+    haps
 end
