9891e09e842e3a8b5085cc55cb1a2527b17e583e
haocheng <reworkhow@gmail.com>
2017-09-17 14:47:41 -0700

    set default residual variance to 0

--- src/global/global.jl
+++ src/global/global.jl
@@ -9,4 +9,4 @@ end
 
 # Make object for storing globals
 G = GenomeInfo(Array{ChromosomeInfo}(0),0,0.0,0.0,[],[])
-common = CommonToAnimals(Array{Animal}(0),G,0,0,1.0)
+common = CommonToAnimals(Array{Animal}(0),G,0,0,0.0)
