b13b1a0a55add1937ff360040648f3b82fc39d96
mdkeehan <21029749+mdkeehan@users.noreply.github.com>
2020-04-24 09:55:03 +1200

    correctly initialise the multi-trait unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -106,6 +106,7 @@ end
 end
 #
 # run a sampleSel with two traits
+# based on the wiki multitrait example
 #
 @testset "selectSel_goldenpath_twoTrait" begin
 	clearGlobals()
@@ -113,22 +114,35 @@ end
 	@test length(XSim.common.G.chr) == 0
 	@test XSim.common.countChromosome == 0 #Number of chromosomes in founder population ; starts at 1
 
-	chrLength= 0.1  #length of each chromosome
-	numChr   = 2    #number of chromosomes
-	nmarkers = 10   #number of loci for each chromosome
-	nQTL     = 1    #number of QTL for each chromosomefects,mutRate);
-	build_genome(numChr,chrLength,nmarkers,nQTL)
+	numChr = 3
+	chrLength = [1.0, 1.1, 0.9]
+	numLoci = [10 ,15 ,20]
+	nTraits = 2
+	geneFreq   = Array{Array{Float64,1},1}(undef,0)
+	qtlIndex  = Array{Array{Int64,1},1}(undef,0)
+	qtlEffects = Array{Array{Float64,nTraits},1}(undef,0)
+	numQTLOnChr =[2, 3, 1]
+	numQTL=sum(numQTLOnChr)
+	qtlIndex = [[1; 4],[3;7;9],[5]]
+	geneFreq = [[.5;.5;.5;.5;.5;.5;.5;.5;.5;.5],
+				[.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5],
+				[.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5]]
+	mPos = [[.05;.1;.15;.2;.25;.3;.35;.40;.45;.5],
+			[.05;.1;.15;.2;.25;.3;.35;.40;.45;.5;.55;.65;.75;.85;.95],
+			[.05;.1;.15;.2;.25;.3;.35;.4;.45;.5;.54;.58;.65;.75;.758;.759;.8;.85;.851;.859]]
+	push!(qtlEffects,randn(2,nTraits))
+	push!(qtlEffects,randn(3,nTraits))
+	push!(qtlEffects,randn(1,nTraits))
+	G0 = [1. 0.5;0.5 2.]
+	XSim.build_genome(numChr,chrLength,numLoci,geneFreq,mPos,qtlIndex,qtlEffects,nTraits,G0)
+	XSim.setResidualVariance(G0)
 
 	popSize = 10
-	ngen = 5
+	ngen = 3
 	popSizeFounder = 20
-	sires = sampleFounders(popSizeFounder);
-	dams  = sampleFounders(popSizeFounder);
+	sires = sampleFounders(popSizeFounder)
+	dams  = sampleFounders(popSizeFounder)
 
-	# initialize for 2 trait
-	A = [10.0 0 ; 0 3.0]
-	XSim.setResidualVariance(cholesky(A).U')
-	
 	sire1,dam1,gen1=sampleSel(popSize, 4, 4, ngen,sires, dams);
 end
 
