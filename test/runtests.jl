d2ebd6c8d97abcebece28b4eed8e31e83d977a48
mdkeehan <21029749+mdkeehan@users.noreply.github.com>
2020-04-24 10:05:57 +1200

    update single trait unittest to follow wiki doc

--- test/runtests.jl
+++ test/runtests.jl
@@ -77,8 +77,7 @@ end
 # 0. clearGlobals
 # 1. build_genome
 # 2. sampleFounders
-# 3. XSim.setResidualVariance
-# 4. sampleSel
+# 3. sampleSel
 #
 @testset "selectSel_goldenpath_oneTrait" begin
 	clearGlobals()
@@ -86,21 +85,35 @@ end
 	@test length(XSim.common.G.chr) == 0
 	@test XSim.common.countChromosome == 0 #Number of chromosomes in founder population ; starts at 1
 
-	chrLength= 0.1  #length of each chromosome
-	numChr   = 2    #number of chromosomes
-	nmarkers = 10   #number of loci for each chromosome
-	nQTL     = 1    #number of QTL for each chromosomefects,mutRate);
-	build_genome(numChr,chrLength,nmarkers,nQTL)
+	numChr=3
+	chrLength = [1.0, 1.1, 0.9]
+	numLoci=[10 ,15 ,20]
+	geneFreq   = Array{Array{Float64,1},1}(undef,0)
+	qtlIndex  = Array{Array{Int64,1},1}(undef,0)
+	qtlEffects = Array{Array{Float64,2},1}(undef,0)
+	numQTLOnChr =[2, 3, 1]
+	numQTL=6
+	qtlIndex = [[1; 4],[3;7;9],[5]]
+	geneFreq = [[.5;.5;.5;.5;.5;.5;.5;.5;.5;.5],
+				[.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5],
+				[.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5;.5]]
+	mPos = [[.05;.1;.15;.2;.25;.3;.35;.40;.45;.5],
+			[.05;.1;.15;.2;.25;.3;.35;.40;.45;.5;.55;.65;.75;.85;.95],
+			[.05;.1;.15;.2;.25;.3;.35;.4;.45;.5;.54;.58;.65;.75;.758;.759;.8;.85;.851;.859]]
+	push!(qtlEffects,randn(2,1))
+	push!(qtlEffects,randn(3,1))
+	push!(qtlEffects,randn(1,1))
+	G0 = Array{Float64,2}(undef,1,1)
+	G0[1,1] = 1
+	nTraits = 1
+	XSim.build_genome(numChr,chrLength,numLoci,geneFreq,mPos,qtlIndex,qtlEffects,nTraits,G0)
+	XSim.setResidualVariance(G0)
 
 	popSize =10
 	ngen =5
 	popSizeFounder = 20
 	sires = sampleFounders(popSizeFounder);
 	dams  = sampleFounders(popSizeFounder);
-
-	# Initilize LRes for 1 trait
-	A = fill(1.0,1,1)
-	XSim.setResidualVariance(cholesky(A).U')
 
 	sire1,dam1,gen1=sampleSel(popSize, 4, 4, ngen,sires, dams);
 end
